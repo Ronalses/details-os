b96008129f00baac6e67e06c5bc77aff7ee87f08
ronalses <ronalses.developer@gmail.com>
2018-10-21 13:05:10 -0300

    geting others details

--- render.js
+++ render.js
@@ -3,7 +3,12 @@ const os = require('os')
 function getInfo(){
 
     let data = {
-        hotname: `${os.hostname()}`
+        hotname: `${os.hostname()}`,
+        cpu: `${os.cpus()[0].model} x ${os.cpus().length}`,
+        ram: `${os.totalmem()}`,
+        arch: `${os.arch() === 'x64' ? '64 Bits' : '32 Bits'}`,
+        platform: `${os.type}`,
+        disk: `${os.freemem()}`
     }
 
     return data
@@ -12,3 +17,8 @@ function getInfo(){
 let dataInfo = getInfo()
 
 document.getElementById('hotname').innerHTML = dataInfo.hotname
+document.getElementById('cpu').innerHTML = dataInfo.cpu
+document.getElementById('ram').innerHTML = dataInfo.ram
+document.getElementById('arch').innerHTML = dataInfo.arch
+document.getElementById('platform').innerHTML = dataInfo.platform
+document.getElementById('disk').innerHTML = dataInfo.disk
