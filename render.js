a1a141bd4fd781172e8a32db4bc53b0f971ee90f
ronalses <ronalses.developer@gmail.com>
2018-10-23 23:29:55 -0300

    get gpu

--- render.js
+++ render.js
@@ -1,6 +1,27 @@
 const os = require('os')
+const { exec } = require('child_process')
 
-function getInfo(){
+function getGpu() {
+    return new Promise((resolve, reject) => {
+        exec('lshw -C display', (err, stdout, string) => {
+            if (err) reject(err)
+
+            let gpuData = stdout.replace('*-display', '').trim()
+
+            let lines = gpuData.split('\n')
+
+            let result = {}
+
+            for (let line of lines) {
+                let keyword = line.split(':')
+                result[keyword[0].trim()] = keyword[1].trim()
+            }
+            resolve(result)
+        })
+    })
+}
+
+function getInfo() {
 
     let data = {
         hotname: `${os.hostname()}`,
@@ -22,3 +43,5 @@ document.getElementById('ram').innerHTML = dataInfo.ram
 document.getElementById('arch').innerHTML = dataInfo.arch
 document.getElementById('platform').innerHTML = dataInfo.platform
 document.getElementById('disk').innerHTML = dataInfo.disk
+
+getGpu().then(data => console.log(data.fabricante))
