dbc5286548bcd319016ad66125d5bdfa58b111c9
ronalses <ronalses.developer@gmail.com>
2018-11-03 19:18:32 -0300

    geting data

--- render.js
+++ render.js
@@ -1,9 +1,13 @@
 const os = require('os')
 
-function getInfo(){
-
+function getInfo() {
     let data = {
-        hotname: `${os.hostname()}`
+        hotname: `${os.hostname()}`,
+        ram: `${os.totalmem()}`,
+        cpu: `${os.cpus()[0].model} x ${os.cpus().length}`,
+        platform: `${os.platform()}`,
+        arch: `${os.arch() === 'x64' ? '64 Bits' : '32 Bits'}`,
+        gpu: `nuestar gpu xD`
     }
 
     return data
@@ -12,3 +16,10 @@ function getInfo(){
 let dataInfo = getInfo()
 
 document.getElementById('hotname').innerHTML = dataInfo.hotname
+document.getElementById('ram').innerHTML = dataInfo.ram
+document.getElementById('cpu').innerHTML = dataInfo.cpu
+document.getElementById('arch').innerHTML = dataInfo.arch
+document.getElementById('platform').innerHTML = dataInfo.platform
+document.getElementById('gpu').innerHTML = dataInfo.gpu
+
+console.log(os.cpus())
